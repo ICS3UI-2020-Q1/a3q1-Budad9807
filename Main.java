aab1cc79e48eb5bb8b44ccc60267e5b41606480e
Budad9807 <Budad9807@wrdsb.ca>
2020-10-01 16:15:10 +0000

    We figured out biggest integer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Finding the biggest integer out of three numbers
+ * @David 
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input 
+    Scanner input = new Scanner(System.in);
+
+    System.out.println("Please enter three integers on separate lines");
+    int choice1 = input.nextInt();
+    int choice2 = input.nextInt();
+    int choice3 = input.nextInt();
+    if(choice1 > choice2){
+     if (choice1 > choice3){
+      System.out.println("choice1");
+     }else {
+      System.out.println("choice3");
+
+     }
+    }else {
+      if(choice3 > choice2){
+        System.out.println("choice3 was the greatest");
+
+
+      }else{
+        System.out.print("choice2");
+      }
+    }
     
   }
 }
+  
